020dfdf6e4149a1bb31eb4c7cb1840cac0ea7a28
sherryqaisrani <hsretra974@gmail.com>
2022-07-30 15:19:58 +0500

    Light Theme done

---
+++ lib/themes/light_theme.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:health_channel_application/utils/colors.dart';
+
+final lightTheme = ThemeData.light().copyWith(
+  primaryColor: aPrimaryColor,
+  scaffoldBackgroundColor: aWhite,
+  backgroundColor: aPlaceHolderColor,
+  textTheme: GoogleFonts.redHatDisplayTextTheme(
+    const TextTheme().copyWith(
+      headline1: TextStyle(
+        fontSize: 48.sp,
+        fontWeight: FontWeight.bold,
+        color: aBlack,
+      ),
+      headline2: TextStyle(
+          fontSize: 24.sp, fontWeight: FontWeight.bold, color: aWhite),
+      headline3: TextStyle(
+        fontSize: 22.sp,
+        color: aWhite,
+      ),
+      headline4: TextStyle(
+        fontSize: 16.sp,
+        color: aWhite,
+      ),
+      headline5: TextStyle(
+        fontSize: 14.sp,
+        color: aWhite,
+      ),
+      headline6: TextStyle(
+        fontSize: 12.sp,
+        color: aWhite,
+      ),
+      bodyText1: TextStyle(
+        fontSize: 11.sp,
+        color: aBlack,
+      ),
+      bodyText2: TextStyle(
+        fontSize: 9.sp,
+        color: aPrimaryColor,
+      ),
+      button: TextStyle(
+        fontSize: 17.sp,
+        fontWeight: FontWeight.bold,
+      ),
+      subtitle1: const TextStyle(
+        color: aBlack,
+      ),
+    ),
+  ),
+);
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 const Color aPrimaryColor = Color(0xffE55E76);
-const Color aPrimaryWhite = Color(0xffFFFFFF);
-const Color aPrimaryBlack = Color(0xff000000);
+const Color aWhite = Color(0xffFFFFFF);
+const Color aBlack = Color(0xff000000);
+const Color aPlaceHolderColor = Color(0xffC1C7D0);
